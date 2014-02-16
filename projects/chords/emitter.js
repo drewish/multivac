4e551e335aad796d727b560ec872bfa2f27da665
andrew morton <drewish@katherinehouse.com>
2014-02-15 23:21:19 -0800

    Cleaned the emitter up and removed the only_once hack

--- projects/chords/emitter.js
+++ projects/chords/emitter.js
@@ -5,46 +5,49 @@ function Emitter() {
 
 Emitter.prototype.on = function(event, callback) {
   this.events[event] = this.events[event] || [];
-  this.events[event].push(callback);
-  return this;
-};
 
-Emitter.prototype.off = function(event, callback) {
-  this.events[event] = this.events[event] || [];
-  if (event in this.events) {
-    this.events[event].splice(this.events[event].indexOf(callback), 1);
-  }
+  this.events[event].push({func: callback, once: false});
+
   return this;
 };
 
 Emitter.prototype.once = function(event, callback) {
   this.events[event] = this.events[event] || [];
-  // Yeah, sticking this variable on the callback is tacky but it beats
-  // sticking it into a hash and then having to find the callback in the array.
-  callback.only_once = true;
-  this.events[event].push(callback);
+
+  this.events[event].push({func: callback, once: true});
+
+  return this;
+};
+
+Emitter.prototype.off = function(event, callback) {
+  if (event in this.events === false) {
+    return this;
+  }
+
+  this.events[event] = this.events[event].filter(function(o) {
+    return o.func !== callback;
+  });
+
   return this;
 };
 
 Emitter.prototype.trigger = function(event /* ...args */) {
-  if (!(event in this.events)) {
-    return;
+  if (event in this.events === false) {
+    return this;
   }
-
-  var self = this;
-  // There will probably be trouble if one of the callback changes the args.
-  var args = Array.prototype.slice.call(arguments, 1);
 
   // Make a copy of the callbacks then remove any one time callbacks, we
   // don't want them called twice if another callback causes the event to
   // fire again.
   var callbacks = this.events[event].slice(0);
-  this.events[event] = this.events[event].filter(function(callback) {
-    return !callback.only_once;
+  this.events[event] = this.events[event].filter(function(o) {
+    return o.once === false;
   });
-  callbacks.forEach(function(callback) {
-    callback.apply(self, args);
-  });
+
+  // There will probably be trouble if one of the callback changes the args,
+  // but this avoids a var so the anonymous function can see it.
+  var args = Array.prototype.slice.call(arguments, 1);
+  callbacks.forEach(function(o) { o.func.apply(this, args); }, this);
 
   return this;
 };
