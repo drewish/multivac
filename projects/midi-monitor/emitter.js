0982768662ca417105a926621eef194534df68ed
andrew morton <drewish@katherinehouse.com>
2014-03-02 14:29:46 -0800

    Backport my emitter fixes

--- projects/midi-monitor/emitter.js
+++ projects/midi-monitor/emitter.js
@@ -5,23 +5,49 @@ function Emitter() {
 
 Emitter.prototype.on = function(event, callback) {
   this.events[event] = this.events[event] || [];
-  this.events[event].push(callback);
+
+  this.events[event].push({func: callback, once: false});
+
+  return this;
+};
+
+Emitter.prototype.once = function(event, callback) {
+  this.events[event] = this.events[event] || [];
+
+  this.events[event].push({func: callback, once: true});
+
   return this;
 };
 
 Emitter.prototype.off = function(event, callback) {
-  this.events[event] = this.events[event] || [];
-  if (event in this.events) {
-    this.events[event].splice(this.events[event].indexOf(callback), 1);
+  if (event in this.events === false) {
+    return this;
   }
+
+  this.events[event] = this.events[event].filter(function(o) {
+    return o.func !== callback;
+  });
+
   return this;
 };
 
 Emitter.prototype.trigger = function(event /* ...args */) {
-  if (event in this.events) {
-    for (var i = 0, len = this.events[event].length; i < len; i++) {
-      this.events[event][i].apply(this, Array.prototype.slice.call(arguments, 1));
-    }
+  if (event in this.events === false) {
+    return this;
   }
+
+  // Make a copy of the callbacks then remove any one time callbacks, we
+  // don't want them called twice if another callback causes the event to
+  // fire again.
+  var callbacks = this.events[event].slice(0);
+  this.events[event] = this.events[event].filter(function(o) {
+    return o.once === false;
+  });
+
+  // There will probably be trouble if one of the callback changes the args,
+  // but this avoids a var so the anonymous function can see it.
+  var args = Array.prototype.slice.call(arguments, 1);
+  callbacks.forEach(function(o) { o.func.apply(this, args); }, this);
+
   return this;
 };
